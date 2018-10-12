b183005ee10d24710078193cb7a97e0a058797de
Morgane Dubus <30866152+mdubus@users.noreply.github.com>
2018-10-12 21:53:21 +0200

    Update monoid.ml

--- d09/ex02/monoid.ml
+++ d09/ex02/monoid.ml
@@ -92,7 +92,3 @@ let () =
   print_endline (string_of_float (Calc_float.power 3.0 3.0));
   print_string "Fact 3 = ";
   print_endline (string_of_float (Calc_float.fact 3.0));
-
-  (* print_endline (string_of_float (Calc_float.power 3.0 3)); *)
-  (* print_endline (string_of_int (Calc_int.mul (Calc_int.add 20 1) 2)); *)
-  (* print_endline (string_of_float (Calc_float.mul (Calc_float.add 20.0 1.0) 2.0)) *)
